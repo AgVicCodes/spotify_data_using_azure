07e6c6fb104197fff887d0dcc63ede894de4935c
AGBELEYE VICTOR <65957206+AgVicCodes@users.noreply.github.com>
2024-09-30 19:17:00 +0100

    Create merge_extract.py

---
+++ merge_extract.py
@@ -0,0 +1,12 @@
+import glob.glob as gg
+import pandas as pd
+
+json_files = gg(f"recently_played*.json")
+
+df = pd.DataFrame()
+
+for file in json_files:
+  temp = pd.json_normalize(file)
+  df = df._append(temp)
+
+print(df.head())
