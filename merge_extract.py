ead37986dac873fa96f0ec9daace225fb9f53eb9
AgVictorCodes <victoragbeleye2@gmail.com>
2024-10-03 23:24:07 +0100

    Edited merge extract file

--- merge_extract.py
+++ merge_extract.py
@@ -1,5 +1,8 @@
-from glob import glob as gg
+import json
+import spotipy
 import pandas as pd
+from extract import sp
+from glob import glob as gg
 
 json_files = gg(f"recently_played*.json")
 
@@ -10,3 +13,14 @@ for file in json_files:
 	df = df._append(temp)
 
 print(df.head())
+
+with open("recently_played1.json", "r") as file:
+    data = json.load(file)
+
+print(data["items"][0]["track"]["album"]["id"])
+
+song_url = data["items"][0]["track"]["album"]["id"]
+
+analytics = sp.audio_analysis(song_url)
+
+print(analytics)
