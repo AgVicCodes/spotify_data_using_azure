6bcd9f3682f070ec4e536e7e97a3b8d6b49a5d80
Agviccodes <victoragbeleye2@gmail.com>
2024-10-10 02:23:07 +0100

    Edited workspace on new OS (mac)

--- extract_recent.py
+++ extract_recent.py
@@ -1,4 +1,5 @@
 import json
+import glob
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
@@ -14,12 +15,22 @@ client = SpotifyOAuth(
 
 sp = spotipy.Spotify(auth_manager = client)
 
+json_files = glob.glob(f"recently_played*.json")
+
+count = len(json_files)
+
+# print(json_files)
+
 def get_recently_played():    
 
     recently_played = sp.current_user_recently_played(limit = 50, after = "1727391600")
 
-    with open("recently_played2.json", "w") as file:
+    with open(f"recently_played{count}.json", "w") as file:
         json.dump(recently_played, file, indent = 4)
+
+
+
+get_recently_played()
 
 # Extract playlist items
 # playlist_items
