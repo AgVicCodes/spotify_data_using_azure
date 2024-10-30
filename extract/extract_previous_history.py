fae686c9fb6dec755ef3c9adde707ae0312a2be7
Agviccodes <victoragbeleye2@gmail.com>
2024-10-30 17:49:07 +0000

    Edited automation file

--- extract/extract_previous_history.py
+++ extract/extract_previous_history.py
@@ -9,9 +9,12 @@ with open("keys.json") as file:
     keys = json.load(file)
 
 client = SpotifyOAuth(
-    client_id = keys["client_id"],
-    client_secret = keys["client_secret_key"],
-    redirect_uri = keys["redirect_uri"],
+    # client_id = keys["client_id"],
+    # client_secret = keys["client_secret_key"],
+    # redirect_uri = keys["redirect_uri"],
+    client_id = keys["CLIENT_ID"],
+    client_secret = keys["CLIENT_SECRET_KEY"],
+    redirect_uri = keys["REDIRECT_URI"],
     scope = "user-read-recently-played"
 )
 
